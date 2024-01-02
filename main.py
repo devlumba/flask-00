25318ea01f531e5d7a618e000c3ac0257b4e1821
devlumba <ilovesylphy@gmail.com>
2024-01-02 20:19:08 +0300

    update from nightx2

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from flask import Flask, render_template, redirect, url_for, request, session, flash
 from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = "SECRET_KEY"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.sqlite3"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.permanent_session_lifetime = timedelta(hours=1)
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+db = SQLAlchemy(app)
 
+# i will remake this whole shit, rn just following the tutorial even though the guy's insane
+class Users(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(40))
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
 
 @app.route("/", methods=["GET"])
 def get_slash():
@@ -36,6 +47,7 @@ def log_in():
         # doesn't work when is unchecked for some reason
         if request.form["remember"]:
             session.permanent = True
+        flash("You successfully logged in", "info")
         return redirect(url_for('get_user', user_id=user))
     else:
         if "user" in session:
@@ -46,5 +58,33 @@ def log_in():
 
 @app.route("/logout", methods=["GET", "POST"])
 def log_out():
-    session.pop("user", None)
+    if "email" in session:
+        email = session["email"]
+        name = session["name"]
+        flash("You successfully have been logged out!", "info")
+        session.pop("name", None)
+        session.pop("email", None)
+    else:
+        flash("You aren't logged in", "info")
     return redirect("/")
+
+
+@app.route("/signup", methods=["GET","POST"])
+def sign_up():
+    if "user" in session or "email" in session:
+        flash("Log out first!", "info")
+        return redirect("/")
+    if request.method == "POST":
+        name = request.form["nm"]
+        email = request.form["email"]
+        session["name"] = name
+        session["email"] = email
+        flash("You have successfully signed up!", "info")
+        return redirect("/")
+    return render_template("sign-up.html")
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True)
