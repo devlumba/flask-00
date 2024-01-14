b3a1bef72bb6be07d7a93e6dbe982eeee191d84f
devlumba <ilovesylphy@gmail.com>
2024-01-15 02:51:57 +0300

    now working, shit was with form.validate_on_submit(), just didn't work with sign_up

--- routes.py
+++ routes.py
@@ -58,8 +58,8 @@ def get_user(user):
 
 
 @app.route("/users/me")
-def get_me(user: User):
-    return render_template("users.html", users=user)
+def get_me():
+    return render_template("users.html", user=current_user)
 
 
 @app.route("/users")
@@ -67,46 +67,16 @@ def get_users():
     return render_template("users.html", users=User.query.all())
 
 
-@app.route("/login", methods=["POST", "GET"])
-def log_in():
-    if request.method == "POST":
-        # user = request.form["nm"]
-        # session["user"] = user
-        # # doesn't work when is unchecked for some reason
-        # if request.form["remember"]:
-        #     session.permanent = True
-        found_user = User.query.filter_by(username=request.form["username"]).first()
-        found_email = User.query.filter_by(email=request.form["email"]).first()
-        if found_user or found_email:
-            user = request.form["username"]
-            email = request.form["email"]
-            session["user"] = user
-            session["email"] = email
-            flash("You successfully signed in")
-            return redirect(url_for("get_user", user=found_user.username))
-        else:
-            flash("Incorrect username of email")
-            return redirect(url_for("log_in"))
-    else:
-        if "user" in session:
-            flash("You already logged in", 'info')
-            return redirect(url_for("get_current_user"))
-    return render_template("login.html")
-
-
 @app.route("/sign_in", methods=["GET", "POST"])
 def sign_in():
     form = SignIn()
     if form.validate_on_submit():
-        found_user = User.query.filter_by(email=form.email.data).first()
-        # validation with check_hashed_password, because it's always random and two hashed password can't be equal, \
-        # even if values were before hashing
-        hashed_password = generate_password_hash(form.password.data).decode("utf-8")
-        if found_user and check_password_hash(hashed_password, found_user.password):
-            login_user(found_user)
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and check_password_hash(user.password, form.password.data):
+            login_user(user)
             # I probably should add remember checkbox, then login_user(user, remember=form.remember.data)
             flash("You have logged successfully")
-            return redirect(url_for("get_me", user=found_user))
+            return redirect(url_for("get_me", user=user))
         else:
             flash("Incorrect email or password")
             return redirect(url_for("sign_in"))
@@ -116,12 +86,12 @@ def sign_in():
 @app.route("/sign_up", methods=["GET", "POST"])
 def sign_up():
     form = SignUp()
-    if form.validate_on_submit():
+    if request.method == "POST":
         found_user = User.query.filter_by(email=form.email.data).first()
         if not found_user:
             hashed_password = generate_password_hash(form.password.data).decode("utf-8")
-            user = User(username=form.username.data, email=form.email.data, password=hashed_password)
             with app.app_context():
+                user = User(username=form.username.data, email=form.email.data, password=hashed_password)
                 db.session.add(user)
                 db.session.commit()
             flash("You have successfully signed up, now you can sign in.")
@@ -132,35 +102,11 @@ def sign_up():
     return render_template("sign_up.html", form=form)
 
 
+
 @app.route("/log_out", methods=["GET", "POST"])
 def log_out():
     logout_user()
     return redirect("/")
-
-
-@app.route("/signup", methods=["GET","POST"])
-def signup():
-    if "user" in session or "email" in session:
-        flash("Log out first!", "info")
-        return redirect("/")
-    if request.method == "POST":
-        # name = request.form["nm"]
-        # email = request.form["email"]
-        # session["name"] = name
-        # session["email"] = email
-        found_user = User.query.filter_by(username=request.form["username"]).first()
-        found_email = User.query.filter_by(email=request.form["email"]).first()
-        if found_user or found_email:
-            flash("Username or email is already taken")
-            return redirect(url_for("sign_up"))
-        else:
-            usr = User(username=request.form["username"], email=request.form["email"])
-            with app.app_context():
-                db.session.add(usr)
-                db.session.commit()
-            flash("You have successfully signed up!", "info")
-            return redirect("/")
-    return render_template("sign-up.html")
 
 
 @app.route("/check_form", methods=["GET", "POST"])
@@ -170,7 +116,7 @@ def test_form():
         flash("Successfully filled form")
         return redirect("/users")
     return render_template("test_form.html", form=form)
-
+#
 #
 # with app.app_context():
 #     db.create_all()
